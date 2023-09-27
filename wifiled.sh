a80c7ddef65feecc259705fe5591bfd33299a293
siliconmagician <123476869+siliconmagician@users.noreply.github.com>
2023-09-27 10:22:41 -0500

    Create wifiled.sh

---
+++ wifiled.sh
@@ -0,0 +1,14 @@
+!/bin/sh
+
+PHY_PATH=$(ls /sys/kernel/debug/ieee80211/phy*/mt76 -d)
+echo 0x770 > ${PHY_PATH}/regidx
+
+while true; do
+    # Check if wlan0 is connected
+    if iw dev wlan0 link | grep -q "Connected"; then
+        echo 0x840000 > ${PHY_PATH}/regval
+    else
+        echo 0x820000 > ${PHY_PATH}/regval
+    fi
+    sleep 5 # Adjust the interval as needed
+done
