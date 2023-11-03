7e7de80d485634f5534ee2f1f64e5c21c2cb3216
Ryster's Tech YT <123476869+RysterTech@users.noreply.github.com>
2023-11-03 14:50:09 -0500

    Update wifiled.sh
    Added blinking on and off every second when no connection is found

--- wifiled.sh
+++ wifiled.sh
@@ -8,7 +8,7 @@ while true; do
     if iw dev wlan0 link | grep -q "Connected"; then
         echo 0x840000 > ${PHY_PATH}/regval
     else
-        echo 0x820000 > ${PHY_PATH}/regval
+        echo 0x820000 > ${PHY_PATH}/regval && sleep 1 && echo 0x800000 > ${PHY_PATH}/regval
     fi
-    sleep 5 # Adjust the interval as needed
+    sleep 1 # Adjust the interval as needed
 done
